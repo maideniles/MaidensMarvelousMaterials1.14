5d2138d7adb5293ee599fdf7c78fe4f5d20b9973
Maideniles <maideniles@gmail.com>
2020-11-19 09:47:10 -0600

    fixed init package placement

--- src/main/java/com/maideniles/maidensmaterials/block/leaves/BlockCrabappleLeaves.java
+++ src/main/java/com/maideniles/maidensmaterials/block/leaves/BlockCrabappleLeaves.java
@@ -1,8 +1,6 @@
 package com.maideniles.maidensmaterials.block.leaves;
 
-import init.ItemInit;
-import init.ModBlocks;
-import init.ModItems;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LeavesBlock;
@@ -32,10 +30,10 @@ public class BlockCrabappleLeaves extends LeavesBlock {
 
         if (!worldIn.isRemote()) {
 
-            if (shears.getItem() == ModItems.PRUNING_SHEARS) {
+            if (shears.getItem() == init.ModItems.PRUNING_SHEARS) {
 
 
-                ItemEntity blossom = new ItemEntity(worldIn, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(ModItems.CRABAPPLE_BLOSSOMS, 1));
+                ItemEntity blossom = new ItemEntity(worldIn, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(init.ModItems.CRABAPPLE_BLOSSOMS, 1));
                 worldIn.addEntity(blossom);
 
 
@@ -46,7 +44,7 @@ public class BlockCrabappleLeaves extends LeavesBlock {
 
 
                 if ((new Random().nextInt(100) + 1) < 10) {
-                    ItemEntity itemessence = new ItemEntity(worldIn, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(ItemInit.FLORAL_ESSENCE.get(), 1));
+                    ItemEntity itemessence = new ItemEntity(worldIn, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(init.ItemInit.FLORAL_ESSENCE.get(), 1));
                     itemessence.setPickupDelay(0);
                     worldIn.addEntity(itemessence);
                 }
